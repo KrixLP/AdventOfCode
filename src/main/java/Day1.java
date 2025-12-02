67a1e6bc6f078fd591dd3dcc56106e18d1aedc78
KrixLP <35309917+KrixLP@users.noreply.github.com>
2025-12-02 18:12:17 +0100

    Solve Day1

---
+++ src/main/java/Day1.java
@@ -0,0 +1,56 @@
+public class Day1 {
+    static int dialPosition = 50;
+    static int zeroes = 0;
+
+    public static void part1(String[] args) {
+        for (String arg : args) {
+            int turnDistance = Integer.parseInt(arg.substring(1));
+            if (arg.startsWith("L")) {
+                turnDistance = -turnDistance;
+            }
+            dialPosition += turnDistance;
+            dialPosition = dialPosition % 100;
+            if (dialPosition == 0) {
+                zeroes++;
+            }
+        }
+        System.out.println(zeroes);
+    }
+
+    public static void part2(String[] args) {
+        for (String arg : args) {
+            int turnDistance = Integer.parseInt(arg.substring(1));
+            if (arg.startsWith("R")) {
+                turnRightPt2(turnDistance);
+            } else if (arg.startsWith("L")) {
+                turnLeftPt2(turnDistance);
+            }
+
+        }
+        System.out.println(zeroes);
+    }
+
+    private static void turnLeftPt2(int turnDistance) {
+        while (turnDistance > 0) {
+            dialPosition = dialPosition - 1;
+            turnDistance--;
+            if (dialPosition == 0) {
+                zeroes++;
+            }
+            if (dialPosition < 0) {
+                dialPosition = dialPosition + 100;
+            }
+        }
+    }
+
+    private static void turnRightPt2(int turnDistance) {
+        while (turnDistance > 0) {
+            dialPosition = dialPosition + 1;
+            turnDistance--;
+            if (dialPosition == 100) {
+                zeroes++;
+                dialPosition = 0;
+            }
+        }
+    }
+}
